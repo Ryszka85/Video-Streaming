10c4de2bc099612184c2423dcffa393eacfb96b0
Adrian Ryszka <adrian.ryszka@gmx.net>
2021-10-24 23:18:08 +0200

    foo10

--- src/app/testing.service.ts
+++ src/app/testing.service.ts
@@ -10,6 +10,7 @@ export class TestingService {
   constructor(private httpClient: HttpClient) { }
 
   public test(): Observable<any> {
+    console.log('HuHu');
     return this.httpClient.get('https://77.116.152.165:8443/test');
   }
 }
